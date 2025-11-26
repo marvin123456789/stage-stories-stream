067012b1dee17a412be839163768f703be00d9a9
Marv <grosse_marvin@web.de>
2025-11-26 18:42:03 +0100

    VODs

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 require('dotenv').config()
 const Mux = require("@mux/mux-node")
 
-const mux = new Mux({
-    tokenId: "e48865f9-7125-4ca8-8e63-3da99a38afa1",
-    tokenSecret: "S2E0Pk03C36uCkm99NJcLWzk5oWHyRE4quS9IbAaejOfsZ95DnXU1l1U5moucPez2liZS2TP+Dm"
-})
 
 const run = async () => {
 
